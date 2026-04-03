bc2f674bd79533d16f8a8a7001da676e756677cd
luudinhhy <luudinhhy06@gmail.com>
2026-04-03 14:29:08 +0700

    Bài tập c++ tính diện tích hình chu nhat

---
+++ test.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+    int main() {
+        float length,  width ;
+        const float PI =3.14159 ; // length luon lon hon width
+        cout <<"nhap length";
+        cin>>length;
+        cout << " nhap  width ";
+        cin>>width;
+        float S=( length * width);
+        cout<<" dien tich hinh chu nhat la:" << endl;
+        return 0;
+
+
+ 
+    }
+    
